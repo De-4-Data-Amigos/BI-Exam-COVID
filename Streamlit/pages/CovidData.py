2b6f70da50861544836c2b1c8a4df9886118b6f4
Kussemar <marcus.dofinsmurt@gmail.com>
2024-03-13 13:36:27 +0100

    Hypotese 1 WIP

--- Streamlit/pages/CovidData.py
+++ Streamlit/pages/CovidData.py
@@ -12,5 +12,5 @@ st.title("Covid Data")
 st.markdown("This application is a Streamlit dashboard that can be used to analyze Covid data.")
 st.markdown("This is our exam project for the course of Business Intelligence 2024.")
 
-gdp_per_capita = st.number_input("GPD pr capita", step=1.0, format="%.2f")
+gdp_per_capita = st.number_input("GNP pr capita", step=1.0, format="%.2f")
 st.write("The number is:", gdp_per_capita)
